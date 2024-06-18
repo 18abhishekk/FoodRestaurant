5586a80154c3c49267b029b0ebe503f5ce9ca3a3
Abhishek <abhishekgaikwad1403@gmail.com>
2024-06-18 11:31:41 +0530

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact:</h1>
+      <h3>Mobile No: 9967983167</h3>
+      <h3>Email: abhishekgaikwad1403@gmail.com</h3>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/Components/utils/constants.js
@@ -0,0 +1,11 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All";
+
+export const MENU_API =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=";
+
+export const RES_API =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
