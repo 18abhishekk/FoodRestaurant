fac7db0b727698da7eff00d09d229c6c0ccf2c85
Abhishek Gaikwad <99329624+18abhishekk@users.noreply.github.com>
2024-06-18 12:34:05 +0530

    Update constants.js

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All";
+  "https://img.lovepik.com/free-png/20220125/lovepik-food-restaurant-logo-png-image_401724748_wh1200.png";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=";
