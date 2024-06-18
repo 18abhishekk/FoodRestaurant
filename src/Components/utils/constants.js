a3b18c94fb90f0fd8ba3bd8f985da23012ea2c5c
Abhishek Gaikwad <99329624+18abhishekk@users.noreply.github.com>
2024-06-18 12:36:41 +0530

    Update constants.js

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
-  "https://img.lovepik.com/free-png/20220125/lovepik-food-restaurant-logo-png-image_401724748_wh1200.png";
+  "https://img.freepik.com/premium-vector/chef-food-restaurant-logo_7085-179.jpg";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=";
